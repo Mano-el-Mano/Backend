8bf94729091c09047124b7ba6c6f9e8af3416b2c
JonasGroenbek <jonasgroenbek@gmail.com>
2020-10-03 15:22:15 +0200

    changed scripts

--- db/pg/scripts/create-user.sh
+++ db/pg/scripts/create-user.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-echo INSERTPASSWORD | sudo -S -u postgres psql -f "${SCRIPTPATH}/create_user.sql"
+sudo -S -u postgres psql -f "${SCRIPTPATH}/create_user.sql"
--- db/pg/scripts/populate-db.sh
+++ db/pg/scripts/populate-db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-echo INSERTPASSWORD | sudo -S -u postgres psql -f "${SCRIPTPATH}/populate_db.sql"
+sudo -S -u postgres psql -f "${SCRIPTPATH}/populate_db.sql"
